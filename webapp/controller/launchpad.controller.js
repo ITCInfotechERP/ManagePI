d886a16db7b2e6933368a03a837b78c029b72b9d
kunjbiharishukla <kunjbihari.shukla@itcinfotech.com>
2018-11-19 13:19:56 +0000

    Kunj: launchpad link changes

--- webapp/controller/launchpad.controller.js
+++ webapp/controller/launchpad.controller.js
@@ -1,20 +1,23 @@
 sap.ui.define([
 	"com/itcActivitybook/controller/BaseController"
-], function(BaseController) {
+], function (BaseController) {
 	"use strict";
 
 	return BaseController.extend("com.itcActivitybook.controller.launchpad", {
 
-		
-		navToDailyActivity: function(oEvent) {
+		navToDailyActivity: function (oEvent) {
 			this.getRouter().navTo("activies");
 		},
-		
-			navToReports: function(oEvent) {
+
+		navToReports: function (oEvent) {
 			this.getRouter().navTo("reports");
 		},
 		
-		navToEditActivity: function(oEvent){
+		onATEPress: function(){
+			sap.m.URLHelper.redirect("http://alturki.com/Main.aspx", true);
+		},
+
+		navToEditActivity: function (oEvent) {
 			this.getRouter().navTo("booklist");
 		}
 	});
