b326e1ebdafd7908a4dcae4817af099fa48df81a
naveensharmaagni <naveenkumar.sharma@itcinfotech.com>
2018-11-19 14:01:11 +0000

    Naveen: Edit button navigation and project detail form applied

--- webapp/controller/launchpad.controller.js
+++ webapp/controller/launchpad.controller.js
@@ -1,23 +1,20 @@
 sap.ui.define([
 	"com/itcActivitybook/controller/BaseController"
-], function (BaseController) {
+], function(BaseController) {
 	"use strict";
 
 	return BaseController.extend("com.itcActivitybook.controller.launchpad", {
 
-		navToDailyActivity: function (oEvent) {
+		
+		navToDailyActivity: function(oEvent) {
 			this.getRouter().navTo("activies");
 		},
-
-		navToReports: function (oEvent) {
+		
+			navToReports: function(oEvent) {
 			this.getRouter().navTo("reports");
 		},
 		
-		onATEPress: function(){
-			sap.m.URLHelper.redirect("http://alturki.com/Main.aspx", true);
-		},
-
-		navToEditActivity: function (oEvent) {
+		navToEditActivity: function(oEvent){
 			this.getRouter().navTo("booklist");
 		}
 	});
